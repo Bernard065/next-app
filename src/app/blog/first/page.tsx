73973e495c0b968a49d03d5fd177978114819b04
+ <+>
2024-06-11 16:04:51 +0300

    catch all segments in next.js

--- src/app/blog/first/page.tsx
+++ src/app/blog/first/page.tsx
@@ -1,3 +1,8 @@
-export default function FirstBlog() {
+export default function FirstBlog({ 
+    params 
+}: {
+    params: {
+        slug:string[];
+    }}) {
     return <h1>My first blog post</h1>
 }
---
+++ src/app/products/[productId]/reviews/page.tsx
@@ -0,0 +1,10 @@
+export default function Reviews() {
+    return (
+        <>
+            <h1>Reviews</h1>
+            <h2>Review1</h2>
+            <h2>Review2</h2>
+            <h2>Review3</h2>
+        </>
+    )
+}
