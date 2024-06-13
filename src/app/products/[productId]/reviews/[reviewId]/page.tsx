d0c4800149d08b6ee8e4d3924bcc90523a447ef4
+ <+>
2024-06-13 09:26:54 +0300

    error loading in nextjs

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,3 +1,7 @@
+const getRandomInt = (count: number) => {
+    return Math.floor(Math.random() * count);
+}
+
 export default function ReviewDetails({ 
     params, 
 }: {
@@ -6,5 +10,11 @@ export default function ReviewDetails({
         reviewId: string;
     };
 }) {
+    const random = getRandomInt(2);
+
+    if (random === 1) {
+        throw new Error("Error loading review")
+    }
+
     return <h1>Review {params.reviewId} for product {params.productId}</h1>
 }
